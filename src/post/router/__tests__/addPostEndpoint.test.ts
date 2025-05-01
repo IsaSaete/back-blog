a63a04fe70deb2743822ce7b68da787d7b8a4043
Isabel Sáenz <isasaete@gmail.com>
2025-05-01 23:26:45 +0200

    Add test to endpoint deletePost

--- src/post/router/__tests__/addPostEndpoint.test.ts
+++ src/post/router/__tests__/addPostEndpoint.test.ts
@@ -4,7 +4,7 @@ import request from "supertest";
 import connectToDatabase from "../../../database/connectToDatabase.js";
 import { huevosRotosBruc159PostData } from "../../postDataFixtures.js";
 import app from "../../../server/app.js";
-import { AddResponsBody, ResponseBodyError } from "../../types.js";
+import { ResponseBodyPost, ResponseBodyError } from "../../types.js";
 import Post from "../../model/Post.js";
 import { huevosRotosBruc159Post } from "../../fixtures.js";
 
@@ -24,7 +24,7 @@ afterAll(async () => {
 });
 
 describe("Given the POST /posts endpoint", () => {
-  describe("When it receives a resquest with Huevos Rotos: el mejor plato de Bruc, 159 data", () => {
+  describe("When it receives a request with Huevos Rotos: el mejor plato de Bruc, 159 data", () => {
     test("Then it should respond with a 201 status code and  Huevos Rotos: el mejor plato de Bruc, 159 post", async () => {
       const expectedStatus = 201;
 
@@ -32,7 +32,7 @@ describe("Given the POST /posts endpoint", () => {
 
       const response = await request(app).post("/posts").send(newPost);
 
-      const body = response.body as AddResponsBody;
+      const body = response.body as ResponseBodyPost;
 
       expect(response.status).toBe(expectedStatus);
       expect(body.post).toEqual(
